b2d4b2b1005a245916f7d7d556473190211faaf6
kunal290314 <knlknsl@gmail.com>
2024-12-07 16:04:41 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "thirdweb SDK + Next starter",
+  description:
+    "Starter template for using thirdweb SDK with Next.js App router",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"></Script>
+      </head>
+      <body className={inter.className}>
+        <ThirdwebProvider>{children}</ThirdwebProvider>
+      </body>
+    </html>
+  );
+}
